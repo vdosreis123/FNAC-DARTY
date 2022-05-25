be6a887ea080d39385aa71acb4d4725a30170d21
DOSREIS <victor.dosreis@fidesio.com>
2022-05-25 11:59:39 +0200

    Prermière Version

---
+++ src/test/java/LOCALISER/LocaliserNonPassant.java
@@ -0,0 +1,20 @@
+package LOCALISER;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LocaliserNonPassant {
+		
+  @Test
+  public void f() {  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
---
+++ src/test/java/RECHERCHER/RechercherPassant.java
@@ -0,0 +1,28 @@
+package RECHERCHER;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class RechercherPassant {
+	private WebDriver driver;
+	
+  @Test
+  public void f() {
+	  driver.get("https://www.fnacdarty.com/");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\DOS REIS\\eclipse-workspace\\FNAC\\src\\test\\resources\\drivers\\chromedriver.exe"); 
+	  driver = new ChromeDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
