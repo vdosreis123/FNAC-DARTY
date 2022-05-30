35ccab4dd495ed1b76bca2d1b2c3de177dc027ac
DOSREIS <victor.dosreis@fidesio.com>
2022-05-30 15:26:10 +0200

    Version 3

---
+++ src/test/java/LOGIN/LoginNonPassant.java
@@ -0,0 +1,19 @@
+package LOGIN;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LoginNonPassant {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
