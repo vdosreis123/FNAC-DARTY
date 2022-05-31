de5d722b9fb22483bdfef10334b2d7b0b069d645
DOSREIS <victor.dosreis@fidesio.com>
2022-05-31 10:48:09 +0200

    Version 4

--- src/test/java/LOGIN/LoginPassant.java
+++ src/test/java/LOGIN/LoginPassant.java
@@ -2,9 +2,12 @@ package LOGIN;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
 public class LoginPassant {
+  private WebDriver driver;
+  
   @Test
   public void f() {
   }
